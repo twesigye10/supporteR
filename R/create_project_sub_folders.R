eabbb1319b871f3df01c98be7f0ac85e5032e5ae
Anthony Twesigye <twesigye10@gmail.com>
2022-07-15 11:16:57 +0300

    documenting the function: create_project_sub_folders

--- R/create_project_sub_folders.R
+++ R/create_project_sub_folders.R
@@ -1,4 +1,15 @@
-create_project_sub_folders <- function(input_sub_folders = c("outputs", "inputs", "R", "support_files")) {
+#' Create sub folders for a project in RStudio
+#'
+#' @param input_sub_folders a vector of strings representing names of folders.
+#' The default folders are c("inputs", "outputs", "R", "support_files")
+#'
+#' @return
+#' @export
+#'
+#' @examples create_project_sub_folders(input_sub_folders = c("inputs", "outputs", "R", "support_files"))
+#' This will create the sub folders folders "inputs", "outputs", "R", "support_files" in the current R project
+#'
+create_project_sub_folders <- function(input_sub_folders = c("inputs", "outputs", "R", "support_files")) {
   sub_directories <- input_sub_folders
   # iterate the sub folders and create them
   for(x in sub_directories){
@@ -8,11 +19,6 @@ create_project_sub_folders <- function(input_sub_folders = c("outputs", "inputs"
     if (check_folder == FALSE) {
       dir.create(paste0("./",x), showWarnings = FALSE)
       message(paste("Folder created :", x))
-      # add ".gitkeep" files to maintain folders while pushing code to github.
-      # works with gitignore
-      if (x %in% c("outputs", "inputs")){
-        file.create(paste0("./",x,"/.gitkeep"))
-      }
     } else{
       warning(paste("Folder already exists :", x))
     }
