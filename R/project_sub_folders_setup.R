c68e87e12c280a6974f85608db74629b1d98bae4
Anthony Twesigye <twesigye10@gmail.com>
2022-07-15 12:20:44 +0300

    renamed file

---
+++ R/project_sub_folders_setup.R
@@ -0,0 +1,57 @@
+#' Create sub folders for a project in RStudio
+#'
+#' @param input_sub_folders a vector of strings representing names of sub folders to be created.
+#' The default folders are "inputs", "outputs", "R", "support_files"
+#'
+#' @return
+#' @export
+#'
+#' @examples create_project_sub_folders(input_sub_folders = c("inputs", "outputs", "R", "support_files"))
+#' # This will create the sub folders folders "inputs", "outputs", "R", "support_files" in the current R project
+#'
+create_project_sub_folders <- function(input_sub_folders = c("inputs", "outputs", "R", "support_files")) {
+  sub_directories <- input_sub_folders
+  # iterate the sub folders and create them
+  for(x in sub_directories){
+    # check folder existence
+    check_folder <- dir.exists(paste0(getwd(),"/",x))
+
+    if (check_folder == FALSE) {
+      dir.create(paste0("./",x), showWarnings = FALSE)
+      message(paste("Folder created :", x))
+    } else{
+      warning(paste("Folder already exists :", x))
+    }
+  }
+}
+
+
+#' Add .gitkeep file to ignored sub folders in .gitignore
+#'
+#' @param input_sub_folders Sub folders to be maintained as empty folders in the project structure while committing
+#' The default folders are "inputs", "outputs"
+#'
+#' @return
+#' @export
+#'
+#' @examples create_dot_gitkeep(input_sub_folders = c("inputs", "outputs"))
+#' # This will add .gitignore in the sub folders "inputs", "outputs"
+#'
+create_dot_gitkeep <- function(input_sub_folders = c("inputs", "outputs")) {
+  sub_directories <- input_sub_folders
+  # iterate the sub folders and add the required file
+  for(x in sub_directories){
+    # check folder existence
+    check_folder <- dir.exists(paste0(getwd(),"/",x))
+
+    if (check_folder == TRUE) {
+      file_path <- paste0("./",x,"/.gitkeep")
+      if(!file.exists(file_path)){
+        file.create(file_path)
+        message(paste("added .gitkeep to :", x))
+      }
+    } else{
+      warning(paste("Folder does not exist :", x))
+    }
+  }
+}
