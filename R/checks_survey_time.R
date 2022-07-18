5abb6987d4a09f697b01401f5cda580592b0b5d2
Anthony Twesigye <twesigye10@gmail.com>
2022-07-18 09:14:53 +0300

    Check interval between surveys

--- R/checks_survey_time.R
+++ R/checks_survey_time.R
@@ -42,3 +42,47 @@ check_survey_time <- function(input_tool_data, input_min_time, input_max_time) {
     dplyr::select(starts_with("i.check")) %>%
     rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
 }
+
+
+#' Check interval between surveys by the same enumerator
+#'
+#' @param input_tool_data Specify the data frame for the tool data
+#' @param input_min_time Specify the minimum expected time for an enumerator to move from one survey location to another
+#'
+#' @return The resulting data frame of surveys not meeting this requirement
+#' @export
+#'
+#' @examples
+#'
+check_time_interval_btn_surveys <- function(input_tool_data, input_min_time) {
+  input_tool_data %>%
+    mutate(i.check.uuid = `_uuid`,
+           i.check.start_date = as_date(start),
+           i.check.enumerator_id = enumerator_id,
+           i.check.district_name = district_name,
+           i.check.point_number = point_number,
+           start = as_datetime(start),
+           end = as_datetime(end)) %>%
+    group_by(i.check.start_date, i.check.enumerator_id) %>%
+    filter(n()>1) %>%
+    arrange(start, .by_group = TRUE) %>%
+    mutate(int.time_between_survey = lubridate::time_length(start - lag(end, default = first(start)), unit = "min"),
+           int.time_between_survey = ceiling(int.time_between_survey)) %>%
+    filter(int.time_between_survey != 0 & int.time_between_survey < input_min_time) %>%
+    mutate(i.check.type = "remove_survey",
+           i.check.name = "hh_id",
+           i.check.current_value = "",
+           i.check.value = "",
+           i.check.issue_id = "less_time_btn_surveys",
+           i.check.issue = glue("{int.time_between_survey} min taken between surveys"),
+           i.check.other_text = "",
+           i.check.checked_by = "",
+           i.check.checked_date = as_date(today()),
+           i.check.comment = "",
+           i.check.reviewed = "",
+           i.check.adjust_log = "",
+           i.check.uuid_cl = "",
+           i.check.so_sm_choices = "") %>%
+    dplyr::select(starts_with("i.check")) %>%
+    rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
+}
