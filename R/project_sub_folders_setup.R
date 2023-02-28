41cb0028685010c36e0063670f095b9f593201d2
Anthony Twesigye <twesigye10@gmail.com>
2023-02-28 14:11:21 +0300

    add gitkeep to support_files as well

--- R/project_sub_folders_setup.R
+++ R/project_sub_folders_setup.R
@@ -35,10 +35,10 @@ create_project_sub_folders <- function(input_sub_folders = c("inputs", "outputs"
 #' @return
 #' @export
 #'
-#' @examples add_dot_gitkeep_to_folder(input_sub_folders = c("inputs", "outputs"))
+#' @examples add_dot_gitkeep_to_folder(input_sub_folders = c("inputs", "outputs", "support_files))
 #' # This will add .gitkeep in the sub folders "inputs", "outputs"
 #'
-add_dot_gitkeep_to_folder <- function(input_sub_folders = c("inputs", "outputs")) {
+add_dot_gitkeep_to_folder <- function(input_sub_folders = c("inputs", "outputs", "support_files")) {
   sub_directories <- input_sub_folders
   # iterate the sub folders and add the required file
   for(x in sub_directories){
