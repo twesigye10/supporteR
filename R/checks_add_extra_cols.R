496d3f1c600817eec6ab0cae9cbcab794095bc5b
Anthony Twesigye <twesigye10@gmail.com>
2023-03-09 11:51:09 +0300

    function: checks_add_extra_cols

---
+++ R/checks_add_extra_cols.R
@@ -0,0 +1,20 @@
+#' Add extra columns to harmonise checking
+#'
+#' @param input_tool_data Specify the data frame for the tool data
+#' @param input_enumerator_id_col Specify the enumerator id column
+#' @param input_location_col Specify the location description column
+#' @param input_point_id_col Specify the point id column
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+checks_add_extra_cols <- function(input_tool_data, input_enumerator_id_col, input_location_col, input_point_id_col) {
+  input_tool_data %>%
+    mutate(i.check.uuid = `_uuid`,
+           i.check.start_date = as_date(start),
+           !!paste0("i.check.", input_enumerator_id_col) := as.character(!!sym(input_enumerator_id_col)),
+           !!paste0("i.check.", input_location_col) := !!sym(input_location_col),
+           !!paste0("i.check.", input_point_id_col) := !!sym(input_point_id_col))
+}
