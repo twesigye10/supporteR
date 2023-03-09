a5b1f1bc576c7481d88e0a3d238c69b81079cd60
Anthony Twesigye <twesigye10@gmail.com>
2023-03-09 10:51:46 +0300

    updated spatial checks scripts

--- R/checks_spatial.R
+++ R/checks_spatial.R
@@ -1,30 +1,37 @@
 #' Check duplicate point numbers
 #'
 #' @param input_tool_data Specify the data frame for the tool data
+#' @param input_enumerator_id_col Specify the enumerator id column
+#' @param input_location_col Specify the location description column
+#' @param input_point_id_col Specify the point id column
 #' @param input_sample_pt_nos_list Specify a list of sample point numbers
 #'
-#' @return Data frame of surveys with duplicate point_number
+#' @return Data frame of surveys with duplicate point ids
 #' @export
 #'
 #' @examples
 #'
-check_duplicate_pt_numbers <- function(input_tool_data, input_sample_pt_nos_list) {
+check_duplicate_pt_numbers <- function(input_tool_data,
+                                       input_enumerator_id_col = "enumerator_id",
+                                       input_location_col,
+                                       input_point_id_col,
+                                       input_sample_pt_nos_list) {
   if("status" %in% colnames(input_tool_data)){
     input_tool_data %>%
       mutate(i.check.uuid = `_uuid`,
              i.check.start_date = as_date(start),
-             i.check.enumerator_id = as.character(enumerator_id),
-             i.check.district_name = district_name,
-             i.check.point_number = point_number) %>%
-      mutate(unique_pt_number = paste0(status, "_", point_number )) %>%
-      group_by(i.check.district_name, status, i.check.point_number) %>%
+             !!paste0("i.check.", input_enumerator_id_col) := as.character(!!sym(input_enumerator_id_col)),
+             !!paste0("i.check.", input_location_col) := !!sym(input_location_col),
+             !!paste0("i.check.", input_point_id_col) := !!sym(input_point_id_col)) %>%
+      mutate(unique_pt_number = paste0(status, "_", !!sym(input_point_id_col) )) %>%
+      group_by(!!paste0("i.check.", input_location_col), status, !!paste0("i.check.", input_point_id_col)) %>%
       filter(n() > 1, unique_pt_number %in% input_sample_pt_nos_list) %>%
       mutate(i.check.type = "change_response",
-             i.check.name = "point_number",
-             i.check.current_value = as.character(point_number),
+             i.check.name = input_point_id_col,
+             i.check.current_value = as.character(!!sym(input_point_id_col)),
              i.check.value = "",
              i.check.issue_id = "spatial_c_duplicate_pt_no",
-             i.check.issue = glue("point_number: {point_number} is duplicated: check that its not a repeated survey"),
+             i.check.issue = paste0(input_point_id_col, ": ", !!sym(input_point_id_col), " is duplicated: check that its not a repeated survey"),
              i.check.other_text = "",
              i.check.checked_by = "",
              i.check.checked_date = as_date(today()),
@@ -39,18 +46,18 @@ check_duplicate_pt_numbers <- function(input_tool_data, input_sample_pt_nos_list
     input_tool_data %>%
       mutate(i.check.uuid = `_uuid`,
              i.check.start_date = as_date(start),
-             i.check.enumerator_id = as.character(enumerator_id),
-             i.check.district_name = district_name,
-             i.check.point_number = point_number) %>%
-      mutate(unique_pt_number = point_number) %>%
-      group_by(i.check.district_name, i.check.point_number) %>%
+             !!paste0("i.check.", input_enumerator_id_col) := as.character(!!sym(input_enumerator_id_col)),
+             !!paste0("i.check.", input_location_col) := !!sym(input_location_col),
+             !!paste0("i.check.", input_point_id_col) := !!sym(input_point_id_col)) %>%
+      mutate(unique_pt_number = !!sym(input_point_id_col)) %>%
+      group_by(!!paste0("i.check.", input_location_col), !!paste0("i.check.", input_point_id_col)) %>%
       filter(n() > 1, unique_pt_number %in% input_sample_pt_nos_list) %>%
       mutate(i.check.type = "change_response",
-             i.check.name = "point_number",
-             i.check.current_value = as.character(point_number),
+             i.check.name = input_point_id_col,
+             i.check.current_value = as.character(!!sym(input_point_id_col)),
              i.check.value = "",
              i.check.issue_id = "spatial_c_duplicate_pt_no",
-             i.check.issue = glue("point_number: {point_number} is duplicated: check that its not a repeated survey"),
+             i.check.issue = paste0(input_point_id_col,": ", !!sym(input_point_id_col)," is duplicated: check that its not a repeated survey"),
              i.check.other_text = "",
              i.check.checked_by = "",
              i.check.checked_date = as_date(today()),
@@ -70,6 +77,9 @@ check_duplicate_pt_numbers <- function(input_tool_data, input_sample_pt_nos_list
 #' Check for point numbers not in samples
 #'
 #' @param input_tool_data Specify the data frame for the tool data
+#' @param input_enumerator_id_col Specify the enumerator id column
+#' @param input_location_col Specify the location description column
+#' @param input_point_id_col Specify the point id column
 #' @param input_sample_pt_nos_list Specify a list of sample point numbers
 #'
 #' @return Data frame of surveys with specified point numbers that are not in the samples
@@ -77,22 +87,26 @@ check_duplicate_pt_numbers <- function(input_tool_data, input_sample_pt_nos_list
 #'
 #' @examples
 #'
-check_pt_number_not_in_samples <- function(input_tool_data, input_sample_pt_nos_list) {
+check_pt_number_not_in_samples <- function(input_tool_data,
+                                           input_enumerator_id_col = "enumerator_id",
+                                           input_location_col,
+                                           input_point_id_col,
+                                           input_sample_pt_nos_list) {
   if("status" %in% colnames(input_tool_data)){
     input_tool_data %>%
       mutate(i.check.uuid = `_uuid`,
              i.check.start_date = as_date(start),
-             i.check.enumerator_id = as.character(enumerator_id),
-             i.check.district_name = district_name,
-             i.check.point_number = point_number) %>%
-      mutate(unique_pt_number = paste0(status, "_", point_number )) %>%
+             !!paste0("i.check.", input_enumerator_id_col) := as.character(!!sym(input_enumerator_id_col)),
+             !!paste0("i.check.", input_location_col) := !!sym(input_location_col),
+             !!paste0("i.check.", input_point_id_col) := !!sym(input_point_id_col)) %>%
+      mutate(unique_pt_number = paste0(status, "_", !!sym(input_point_id_col) )) %>%
       filter(!unique_pt_number %in% input_sample_pt_nos_list) %>%
       mutate(i.check.type = "change_response",
-             i.check.name = "point_number",
-             i.check.current_value = as.character(point_number),
+             i.check.name = input_point_id_col,
+             i.check.current_value = as.character(!!sym(input_point_id_col)),
              i.check.value = "",
              i.check.issue_id = "spatial_c_pt_no_not_in_sample",
-             i.check.issue = glue("point_number: {point_number} not in samples"),
+             i.check.issue = paste0(input_point_id_col,": ",!!sym(input_point_id_col), " not in samples"),
              i.check.other_text = "",
              i.check.checked_by = "",
              i.check.checked_date = as_date(today()),
@@ -106,17 +120,17 @@ check_pt_number_not_in_samples <- function(input_tool_data, input_sample_pt_nos_
     input_tool_data %>%
       mutate(i.check.uuid = `_uuid`,
              i.check.start_date = as_date(start),
-             i.check.enumerator_id = as.character(enumerator_id),
-             i.check.district_name = district_name,
-             i.check.point_number = point_number) %>%
-      mutate(unique_pt_number = point_number) %>%
+             !!paste0("i.check.", input_enumerator_id_col) := as.character(!!sym(input_enumerator_id_col)),
+             !!paste0("i.check.", input_location_col) := !!sym(input_location_col),
+             !!paste0("i.check.", input_point_id_col) := !!sym(input_point_id_col)) %>%
+      mutate(unique_pt_number = !!sym(input_point_id_col)) %>%
       filter(!unique_pt_number %in% input_sample_pt_nos_list) %>%
       mutate(i.check.type = "change_response",
-             i.check.name = "point_number",
-             i.check.current_value = as.character(point_number),
+             i.check.name = input_point_id_col,
+             i.check.current_value = as.character(!!sym(input_point_id_col)),
              i.check.value = "",
              i.check.issue_id = "spatial_c_pt_no_not_in_sample",
-             i.check.issue = glue("point_number: {point_number} not in samples"),
+             i.check.issue = paste0(input_point_id_col, ": ", !!sym(input_point_id_col), "not in samples"),
              i.check.other_text = "",
              i.check.checked_by = "",
              i.check.checked_date = as_date(today()),
@@ -135,6 +149,9 @@ check_pt_number_not_in_samples <- function(input_tool_data, input_sample_pt_nos_
 #'
 #' @param input_sample_data Specify a GIS layer with sample data
 #' @param input_tool_data Specify the data frame for the tool data with "_geopoint_longitude" and "_geopoint_latitude" columns
+#' @param input_enumerator_id_col Specify the enumerator id column
+#' @param input_location_col Specify the location description column
+#' @param input_point_id_col Specify the point id column
 #' @param input_threshold_dist Specify threshold distance. Default is 150m
 #'
 #' @return Data frame with surveys collected at a distance greater than the specified threshold distance
@@ -142,7 +159,12 @@ check_pt_number_not_in_samples <- function(input_tool_data, input_sample_pt_nos_
 #'
 #' @examples
 #'
-check_threshold_distance <- function(input_sample_data, input_tool_data, input_threshold_dist = 150) {
+check_threshold_distance <- function(input_sample_data,
+                                     input_tool_data,
+                                     input_enumerator_id_col = "enumerator_id",
+                                     input_location_col,
+                                     input_point_id_col,
+                                     input_threshold_dist = 150) {
 
   if("status" %in% colnames(input_tool_data)){
     df_sample_data_thresh <- input_sample_data %>%
@@ -150,7 +172,7 @@ check_threshold_distance <- function(input_sample_data, input_tool_data, input_t
       sf::st_transform(4326)
 
     df_tool_data_thresh <- input_tool_data %>%
-      mutate(unique_pt_number = paste0(status, "_", point_number )) %>%
+      mutate(unique_pt_number = paste0(status, "_", !!sym(input_point_id_col) )) %>%
       sf::st_as_sf(coords = c("_geopoint_longitude","_geopoint_latitude"), crs = 4326)
   }else{
     df_sample_data_thresh <- input_sample_data %>%
@@ -158,7 +180,7 @@ check_threshold_distance <- function(input_sample_data, input_tool_data, input_t
       sf::st_transform(4326)
 
     df_tool_data_thresh <- input_tool_data %>%
-      mutate(unique_pt_number = point_number) %>%
+      mutate(unique_pt_number = !!sym(input_point_id_col)) %>%
       sf::st_as_sf(coords = c("_geopoint_longitude","_geopoint_latitude"), crs = 4326)
   }
 
@@ -200,13 +222,13 @@ check_threshold_distance <- function(input_sample_data, input_tool_data, input_t
     df_data_with_distance %>%
       mutate(i.check.uuid = `_uuid`,
              i.check.start_date = as_date(start),
-             i.check.enumerator_id = as.character(enumerator_id),
-             i.check.district_name = district_name,
-             i.check.point_number = point_number) %>%
+             !!paste0("i.check.", input_enumerator_id_col) := as.character(!!sym(input_enumerator_id_col)),
+             !!paste0("i.check.", input_location_col) := !!sym(input_location_col),
+             !!paste0("i.check.", input_point_id_col) := !!sym(input_point_id_col)) %>%
       filter(as.numeric(distance) >= input_threshold_dist) %>%
       mutate(i.check.type = "remove_survey",
-             i.check.name = "point_number",
-             i.check.current_value = as.character(point_number),
+             i.check.name = input_point_id_col,
+             i.check.current_value = as.character(!!sym(input_point_id_col)),
              i.check.value = "",
              i.check.issue_id = "spatial_c_dist_to_sample_greater_than_threshold",
              i.check.issue = glue("{distance} m greater_than_threshold: {input_threshold_dist} m"),
