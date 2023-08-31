782a7d1736fc4501cea1193843823bbbb3a10045
Anthony Twesigye <twesigye10@gmail.com>
2023-08-31 12:52:16 +0300

    updated output point number column

--- R/sample_points_for_kobo.R
+++ R/sample_points_for_kobo.R
@@ -4,6 +4,7 @@
 #'
 #' @param input_gis_pt_layer Specify the layer where to extract data
 #' @param input_pt_no_col Specify the input point number column
+#' @param output_pt_no_col Specify the point number id column to output
 #' @param output_longitude_col Specify the longitude column to output
 #' @param output_latitude_col Specify the latitude column to output
 #' @param output_description_col Specify the description column to output
@@ -15,13 +16,13 @@
 #'
 #' @examples
 samples_for_kobo <- function(input_gis_pt_layer,
-                                     input_pt_no_col = "pt_num",
-                                     output_longitude_col = "longitude",
-                                     output_latitude_col = "latitude",
-                                     output_description_col = "description",
-                                     output_file_name = "sample_pts_data",
-                                     output_folder_name = "outputs"
-                                     ) {
+                             input_pt_no_col = "pt_num",
+                             output_pt_no_col = "pt_num",
+                             output_longitude_col = "longitude",
+                             output_latitude_col = "latitude",
+                             output_description_col = "description",
+                             output_file_name = "sample_pts_data",
+                             output_folder_name = "outputs") {
   # test type of the geometry in the layer
   if(sf::st_is(x = input_gis_pt_layer[1,],type = "POINT")){
     # get coordinate system
@@ -33,10 +34,10 @@ samples_for_kobo <- function(input_gis_pt_layer,
     }
     # add and format columns
     data_extraction <- data_extraction_layer %>%
-    mutate( !!paste0("i.check.", input_pt_no_col) := as.character(!!sym(input_pt_no_col)),
-            !!paste0("i.check.", output_latitude_col) := sf::st_coordinates(.)[,1],
-            !!paste0("i.check.", output_longitude_col) := sf::st_coordinates(.)[,2],
-            !!paste0("i.check.", output_description_col) := as.character(!!sym(input_pt_no_col)) ) %>%
+      mutate( !!paste0("i.check.", output_pt_no_col) := as.character(!!sym(input_pt_no_col)),
+              !!paste0("i.check.", output_latitude_col) := sf::st_coordinates(.)[,1],
+              !!paste0("i.check.", output_longitude_col) := sf::st_coordinates(.)[,2],
+              !!paste0("i.check.", output_description_col) := as.character(!!sym(input_pt_no_col)) ) %>%
       sf::st_drop_geometry() %>%
       batch_select_rename()
     # check if folder exists and if not create it
