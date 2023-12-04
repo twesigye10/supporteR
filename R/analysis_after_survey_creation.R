bdffe129d4d2bdb6d43ad37ca5c07fbe799e2b8f
Anthony Twesigye <twesigye10@gmail.com>
2023-12-04 15:03:46 +0300

    adding a second subset to the analysis

--- R/analysis_after_survey_creation.R
+++ R/analysis_after_survey_creation.R
@@ -1,18 +1,23 @@
 #' Analysis after survey creation
 #' This function helps to analyse the data. It is based on butteR::survey_collapse.
+#'
 #' @param input_svy_obj Input survey object. The object is created using srvyr::as_survey.
 #' @param input_dap Input dap. The dap template has columns 'variable', 'split', 'subset_1'.
+#' @param population_type Population type to add this column to the results
+#' @param main_subset second subset not specified in the dap
 #'
 #' @return
 #' @export
 #'
 #' @examples
-analysis_after_survey_creation <- function(input_svy_obj, input_dap) {
+analysis_after_survey_creation <- function(input_svy_obj, input_dap, population_type = "refugee", main_subset = NULL) {
 
   # store analyses
   outputs <-list()
 
-  # analysis -----------------------------------------------------------------
+
+  # overall, no additional subset -------------------------------------------
+
 
   dap_refugee <- input_dap %>%
     filter(split %in% c("all", "refugee_only"))
@@ -24,7 +29,11 @@ analysis_after_survey_creation <- function(input_svy_obj, input_dap) {
   # overall, no additional subset
   outputs$ref_overall <- butteR::survey_collapse(df = input_svy_obj,
                                                  vars_to_analyze = refugee_variables_no_subsets) %>%
-    mutate(population = "")
+    mutate(population = population_type)
+
+
+  # overall by subset 1 ---------------------------------------
+
 
   #  subsets
   dap_refugee_subset1 <- input_dap %>%
@@ -48,7 +57,39 @@ analysis_after_survey_creation <- function(input_svy_obj, input_dap) {
   }
 
   outputs$ref_overall_subset1 <- bind_rows(ref_overall_subset1) %>%
-    mutate(population = "")
+    mutate(population = population_type)
+
+
+  # overall by main_subset & subset 1 ---------------------------------------
+
+
+  #  subsets
+  dap_refugee_main_subset_subset1 <- input_dap %>%
+    filter(split %in%  c("all","refugee_only"), !is.na(subset_1),
+           !subset_1 %in% c(main_subset))
+
+  # overall, subset 1
+  dap_refugee_main_subset_split <- dap_refugee_main_subset_subset1 %>%
+    split(.$subset_1)
+
+  if(!is.null(main_subset)){
+
+    ref_main_subset_subset1 <- list()
+
+    for(i in seq_along(dap_refugee_main_subset_split)){
+      print(i)
+      subset_temp <-dap_refugee_main_subset_split[[i]]
+      subset_value <- unique(subset_temp$subset_1)
+      vars_temp <- subset_temp %>% pull(variable)
+      ref_main_subset_subset1[[subset_value]] <- butteR::survey_collapse(df = ref_svy,
+                                                                         vars_to_analyze = vars_temp ,
+                                                                         disag = c(subset_value, main_subset)
+      )
+    }
+
+    outputs$ref_main_subset_subset1 <- bind_rows(ref_main_subset_subset1) %>%
+      mutate(population = population_type)
+  }
 
   # merge analysis ----------------------------------------------------------
 
