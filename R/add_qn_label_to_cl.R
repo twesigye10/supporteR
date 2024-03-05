59d41e88df529f0603926012b813e0ebcaa40fc6
Anthony Twesigye <twesigye10@gmail.com>
2024-03-05 09:47:28 +0300

    fix question label

--- R/add_qn_label_to_cl.R
+++ R/add_qn_label_to_cl.R
@@ -17,13 +17,19 @@ add_qn_label_to_cl <- function(input_cl, input_cl_name_col = "question",
     stop("check the columns provided for the cleaning log and tool")
   }
 
+  # filter the tool
+  df_filtered_tool <- input_tool %>%
+    mutate(int.tool_name = !!sym(input_tool_name_col),
+           int.tool_label = !!sym(input_tool_label_col)) %>%
+    filter(!is.na(int.tool_label))
+
   input_cl %>%
     mutate(int.name = !!sym(input_cl_name_col),
            int.name = ifelse(str_detect(string = int.name, pattern = "\\/"),
                              str_replace_all(string = int.name, pattern = "\\/+.*", replacement = ""), int.name),
-           question_label = ifelse(int.name %in% input_tool[[input_tool_name_col]],
-                                   recode(int.name, !!!setNames(input_tool[[input_tool_label_col]], input_tool[[input_tool_name_col]])),
+           question_label = ifelse(int.name %in% df_filtered_tool$int.tool_name,
+                                   recode(int.name, !!!setNames(df_filtered_tool$int.tool_label, df_filtered_tool$int.tool_name)),
                                    int.name)) %>%
     relocate(question_label, .after = !!sym(input_cl_name_col)) %>%
-    select(-int.name)
+    select(-starts_with("int."))
 }
